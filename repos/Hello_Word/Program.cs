74be613133b72f144091edc0c2aa16184ee1cec2
Tamara <60240971+Prussak@users.noreply.github.com>
2021-02-17 18:49:18 -0300

    Add files via upload

---
+++ repos/Hello_Word/Program.cs
@@ -0,0 +1,17 @@
+﻿using System; //Usando a biblioteca System
+using System.Collections.Generic; //Usando a biblioteca System com coleções genéricas
+using System.Linq; // Usando a biblioteca System Linq (transforma o programa em linguagem de maquina)
+using System.Text; //Unsando a biblioteca System Text (ativa recursos de texto)
+using System.Threading.Tasks; // Usando bibliotecas de multiplas linhas de tarefas 
+
+namespace Hello_Word //Nome do projeto
+{
+    class Program //Nome da classe (POO)
+    {
+        static void Main(string[] args) //Metodo principal da classe
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Tamara");
+        }
+    }
+}
---
+++ repos/Proj_Var/Program.cs
@@ -0,0 +1,21 @@
+﻿using System; //Usando biblioteca System
+using System.Collections.Generic; //Usando coleção genérica
+using System.Linq; //Transforma em linguagem de maquina
+using System.Text;//Ativa recursos de texto
+using System.Threading.Tasks; // multiplas linhas de tarefas
+
+namespace Proj_Var //nome do projeto
+{
+    class Program //nome da classe
+    {
+        static void Main(string[] args) //metodo principal
+        {
+            Console.WriteLine("Qual é o seu nome?\n"); 
+            var nome = Console.ReadLine(); // o var paranomear variavel e o read para ler a função
+            var data = DateTime.Now; //coletar data
+            Console.WriteLine($"\n Olá, {nome}, on {data:d} at {data:t}!!!"); //o sifrão aguarda uma variavel
+            Console.Write("\n Pressione algo para sair...");
+            Console.ReadKey(true); // ler uma tecla = verdadeiro
+        }
+    }
+}

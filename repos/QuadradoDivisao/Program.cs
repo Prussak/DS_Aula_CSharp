9568667afc2c1bf16a67e7baca4c2ce8382eee7d
Tamara <60240971+Prussak@users.noreply.github.com>
2021-03-14 22:30:22 -0300

    Add files via upload
    Exercício 15/03/2021: Quadrado da Divisão

---
+++ repos/QuadradoDivisao/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace QuadradoDivisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Digite o valor de A:");
+           int A = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor de B");
+           int B = int.Parse(Console.ReadLine());
+
+            double divisao = A / B;
+            double quadrado = Math.Pow(divisao, 2);
+
+            Console.WriteLine("O resultado do quadrado da divisão é = " + quadrado);
+            Console.ReadKey();
+        }
+    }
+}
